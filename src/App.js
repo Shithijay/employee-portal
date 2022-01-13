237ef887392be0e64a7baa4fe0ce0f374c16bee1
Shithijay <sranganathan0@gmail.com>
2022-01-13 17:18:13 +0530

    Authentication added

--- src/App.js
+++ src/App.js
@@ -13,30 +13,43 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { loadEmployees } from "./actions/action-creators"
 
+//amplify packages
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsEXports from './aws-exports';
+Amplify.configure(awsEXports);
+
 function App({ loadEmployees }) {
   loadEmployees();
   return (
-    <Router>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">{process.env.REACT_APP_APPLICATION_NAME}</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/about">About</Nav.Link>
-            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <div id="pagecontainer">
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/about" element={<About />}></Route>
-          <Route exact path="/contact" element={<Contact />}></Route>
-          <Route exact path="/employees/loc/:locId/ecode/:ecode" element={<EmployeeDetail />}></Route>
-          <Route exact path="/employees/create" element={<EmployeeForm />}></Route>
-        </Routes>
-      </div>
-    </Router>
+    <Authenticator loginMechanisms={['username']}>
+    {({signOut,user})=>(
+      <Router>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">{process.env.REACT_APP_APPLICATION_NAME}</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/about">About</Nav.Link>
+              <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+              <Nav.Link onClick={signOut}>SignOut</Nav.Link>
+              <Nav.Link href="#">Welcome {user.username}</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <div id="pagecontainer">
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/about" element={<About />}></Route>
+            <Route exact path="/contact" element={<Contact />}></Route>
+            <Route exact path="/employees/loc/:locId/ecode/:ecode" element={<EmployeeDetail />}></Route>
+            <Route exact path="/employees/create" element={<EmployeeForm />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    )}
+    </Authenticator>
   );
 }
 
